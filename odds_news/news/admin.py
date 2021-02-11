0a988a4422b54409f0480a98557101b2886c324a
zkan <zkan.cs@gmail.com>
2021-02-11 13:34:50 +0700

    Register News to admin

--- odds_news/news/admin.py
+++ odds_news/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import News
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+    )
