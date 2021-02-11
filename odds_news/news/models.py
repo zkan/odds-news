1423b0ddfdaaa3f012f58fd62af827839e6370c7
zkan <zkan.cs@gmail.com>
2021-02-11 11:11:04 +0700

    Create news model

--- odds_news/news/models.py
+++ odds_news/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
